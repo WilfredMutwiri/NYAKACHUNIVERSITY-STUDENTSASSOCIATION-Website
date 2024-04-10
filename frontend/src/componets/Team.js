c289c758b0a74d90af4a70c6c9b2155c409dfc70
Wilfred Mutwiri <Wilfred Mutwiri>
2024-04-10 12:43:55 +0300

    Add more members

--- frontend/src/componets/Team.js
+++ frontend/src/componets/Team.js
@@ -4,6 +4,9 @@ import secretaryImg from '../images/secretary.webp'
 import TreasurerImg from '../images/Treasurer.webp'
 import spokesPersonImg from '../images/spokesperson.webp'
 import externalAffairs from '../images/externalAffairs.webp'
+import viceChair from '../images/ViceChair.webp'
+import LegalAffairs from '../images/LegalAffairs.webp'
+import SecretaryGeneral from '../images/SecretaryGeneral.webp'
 const Team = () => {
     return (
         <div>
@@ -27,10 +30,10 @@ const Team = () => {
             </div>
             </div>
             <div class="w-52 m-auto mb-10 md:mb-0">
-            <img class="w-full h-72 object-cover m-auto rounded-md" src={secretaryImg} alt='Secretary Img'/>
+            <img class="w-full h-72 object-cover m-auto rounded-md" src={viceChair} alt='viceChair Img'/>
             <div class="bg-gray-300 m-auto p-2">
-            <h2 class="text-orange-500">Kisero Kennedy</h2>
-            <span class="text-blue-700">Organizing Secretary</span>
+            <h2 class="text-orange-500">Lavender Awuor</h2>
+            <span class="text-blue-700">Vice Chair</span>
             </div>
             </div>
             </div>
@@ -58,7 +61,31 @@ const Team = () => {
             </div>
             </div>
             </div>
-            <p class="text-center pt-5 pb-5">We are a full house! Wish to be part of us? Kindly fill the <a class="text-orange-600" href='https://docs.google.com/forms/d/e/1FAIpQLSf9iSUN_-Tqw7AAHpVbEz8-3c1-mGeoNeM5vlQevydI3IyXdA/viewform' target='_blank'>form here</a> and we will get back to you as soon as possible!</p>
+            {/* third grid */}
+            <div class="block md:grid grid-cols-3 pt-0 md:pt-12" id='grid2'>
+            <div class="w-52 m-auto mb-10 md:mb-0">
+            <img class="h-96 w-52 object-cover m-auto rounded-md" src={LegalAffairs} alt='LegalAffairs Img'/>
+            <div class="bg-gray-300 m-auto p-2">
+            <h2 class="text-orange-500">Sir Alfred</h2>
+            <span class="text-blue-700">legal Affairs</span>
+            </div>
+            </div>
+            <div class="w-52 m-auto mb-10 md:mb-0">
+            <img class="h-96 w-52 object-cover m-auto rounded-md" src={SecretaryGeneral} alt='SecretaryGeneral Img'/>
+            <div class="bg-gray-300 m-auto p-2">
+            <h2 class="text-orange-500">Tobias Osano</h2>
+            <span class="text-blue-700">Secretary General</span>
+            </div>
+            </div>
+            <div class="w-52 m-auto mb-5 md:mb-0">
+            <img class="h-96 w-52 object-cover m-auto rounded-md" src={secretaryImg} alt='secretary Img'/>
+            <div class="bg-gray-300 m-auto p-2">
+            <h2 class="text-orange-500">Kisero Kennedy.</h2>
+            <span class="text-blue-700">Organizing Secretary</span>
+            </div>
+            </div>
+            </div>
+            <p class="text-center p-5 md:p-10">We are a full house! Wish to be part of us? Kindly fill the <a class="text-orange-600" href='https://docs.google.com/forms/d/e/1FAIpQLSf9iSUN_-Tqw7AAHpVbEz8-3c1-mGeoNeM5vlQevydI3IyXdA/viewform' target='_blank'>form here</a> and we will get back to you as soon as possible!</p>
             </div>
             </div>
         </div>
